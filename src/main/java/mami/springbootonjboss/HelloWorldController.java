0949ffff6f479a069198c274d008fea447a0937d
Marcin Miotk <mami@snmill.com>
2017-09-05 19:59:32 +0200

    web.xml for JBoss and Controller REST

---
+++ src/main/java/mami/springbootonjboss/HelloWorldController.java
@@ -0,0 +1,29 @@
+package mami.springbootonjboss;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+class HelloWorldController {
+
+    private Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
+
+    @RequestMapping(method = RequestMethod.GET, value = "/hello", produces = "application/json")
+    @ResponseBody
+    public String helloWorld() {
+        System.out.println("MAMI: HELLO WORLD SYSTEM.OUT.PRINTLN");
+
+        logger.info("MAMI: HELLO WORLD SLF4J : INFO");
+        logger.warn("MAMI: HELLO WORLD SLF4J : WARN");
+        logger.error("MAMI: HELLO WORLD SLF4J : ERROR");
+        logger.debug("MAMI: HELLO WORLD SLF4J : DEBUG");
+        logger.trace("MAMI: HELLO WORLD SLF4J : TRACE");
+
+        return "Hello World MaMi";
+    }
+
+}
---
+++ src/main/java/mami/springbootonjboss/SpringBootOnJbossApplication.java
@@ -0,0 +1,36 @@
+package mami.springbootonjboss;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootOnJbossApplication extends SpringBootServletInitializer {
+
+
+	private Logger logger = LoggerFactory.getLogger(SpringBootOnJbossApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOnJbossApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
+
+		System.out.println("MAMI: HELLO WORLD CONFIGURE");
+
+		logger.info("MAMI: HELLO WORLD SLF4J CONFIGURE: INFO");
+		logger.warn("MAMI: HELLO WORLD SLF4J CONFIGURE: WARN");
+		logger.error("MAMI: HELLO WORLD SLF4J CONFIGURE: ERROR");
+		logger.debug("MAMI: HELLO WORLD SLF4J CONFIGURE: DEBUG");
+		logger.trace("MAMI: HELLO WORLD SLF4J CONFIGURE: TRACE");
+
+		return application.sources(applicationClass);
+	}
+
+	private static Class<SpringBootOnJbossApplication> applicationClass = SpringBootOnJbossApplication.class;
+}
